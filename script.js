ed64485b0965fa26ed4c98c31e1abcc5b069288d
K6id3n <kaidengarza203@gmail.com>
2024-02-09 10:41:24 -0800

    JAVASCRIPT HUMBLE BEGGINGS

---
+++ script.js
@@ -0,0 +1,85 @@
+
+let finalCount = 0
+let countAdd = 2
+
+function addCount(){
+
+    finalCount += countAdd;
+    
+    var outPutElement = document.getElementById("final");
+    outPutElement.textContent = (finalCount)
+
+}
+
+function addAdd() {
+
+    if(finalCount < 25) {
+
+        alert("Insuficient Funds")
+
+    }
+
+    if(!(finalCount < 25)) {
+
+            countAdd *= 2; 
+    
+            finalCount -= 25;
+
+            console.log(finalCount);
+         
+            let button = document.getElementById("upgradeBut");
+
+            button.classList.add("hide");
+
+    }
+
+}
+
+
+function addAddDos() {
+
+    if(finalCount < 50) {
+
+        alert("Insuficient Funds")
+
+    }
+
+    if(!(finalCount < 50)) {
+
+            countAdd *= 2; 
+    
+            finalCount -= 50;
+
+            console.log(finalCount);
+         
+            let buttonDos = document.getElementById("upgradeBut2");
+
+            buttonDos.classList.add("hide");
+
+    }
+
+}
+
+function addAddThr() {
+
+    if(finalCount < 100) {
+
+        alert("Insuficient Funds")
+
+    }
+
+    if(!(finalCount < 100)) {
+
+            countAdd *= 2; 
+    
+            finalCount -= 100;  
+
+            console.log(finalCount);
+         
+            let buttonThr = document.getElementById("upgradeBut3");
+
+            buttonThr.classList.add("hide");
+
+    }
+
+}
