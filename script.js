258e892d8f4779d6618669ec2cb0ba60fa8352e9
K6id3n <kaidengarza203@gmail.com>
2024-02-13 23:14:02 -0800

    purchase/inflation function: for bots worked out,
    make universal

--- script.js
+++ script.js
@@ -11,10 +11,18 @@ function addCount() {
 
     finalCount += countAdd;
 
+    //for debug
+    finalCount += 999;
+
     var outPutElement = document.getElementById("final");
     outPutElement.textContent = (finalCount)
 
 }
+
+//notes
+
+//think of implementing gamble?
+//finish dynamic bot buy fun
 
 //achivements
 
@@ -23,23 +31,15 @@ function achivementsOneMillion() {
     if (finalCount >= 1000000) {
 
         alert("Congrats on one million!")
-
         clearInterval(milInterval)
-
         var outPutElement = document.getElementById("oneMil");
         outPutElement.textContent = ("One Million")
-
         countAdd *= 10;
-
         var outPutElement = document.getElementById("multCount");
         outPutElement.textContent = (countAdd)
-
         setInterval(oneMilObj, 100)
-
         outPutElement.textContent = (rateValue)
-
         rateValue += 100000;
-
         var outPutElement = document.getElementById("rate");
         outPutElement.textContent = (rateValue)
 
@@ -50,7 +50,6 @@ function achivementsOneMillion() {
 function oneMilObj() {
 
     finalCount += 10000;
-
     var outPutElement = document.getElementById("final");
     outPutElement.textContent = (finalCount)
 
@@ -126,18 +125,14 @@ let coinInterval = setInterval(coinShow, 90000)
 function coinClick() {
 
     document.getElementById("coin").style.display = "none";
-
     countAdd *= 100; 
-
     var outPutElement = document.getElementById("multCount");
     outPutElement.textContent = countAdd;
 
     function coinEnd() {
 
         countAdd /= 100;
-
         var outPutElement = document.getElementById("multCount");
-
         outPutElement.textContent = countAdd;
 
     }
@@ -160,7 +155,6 @@ function timer() {
             setTimeout(countdown, 1000);
         } 
         if (i === 0) {
-
             var outPutElement = document.getElementById("alertBox");
             outPutElement.textContent = "Boost Over";
             return;
@@ -211,7 +205,7 @@ function addAddFunction(price, mlt, butId) {
 
     }
 
-    if (!(finalCount < price)) {
+    if (finalCount > price) {
 
         countAdd *= mlt;
         finalCount -= price;
@@ -229,63 +223,48 @@ function addAddFunction(price, mlt, butId) {
 
 //objects
 
-let objPrice = 500;
+let objPriceValueOne = 500;
 
-let objPriceDos = 5000;
+let objPriceValueDos = 5000;
 
-let objPriceThr = 10000;
+let objPriceValueThr = 10000;
 
-let botCount = 0;
 
-let botCountDos = 0;
 
-let botCountThr = 0;
+//dynamic buy obj fun
+
+function buyObjFun(objPriceValue) {
+
+    if (finalCount < objPriceValue) {
+        alert("Insuficient Funds");
+        return objPriceValue;
+    } 
+    
+    if (finalCount > objPriceValue) {
+        //purchase/inflation function:
+        finalCount -= objPriceValue;
+        var outPutElement = document.getElementById("final");
+        outPutElement.textContent = (finalCount);
+        objPriceValue *= 1.2;
+        let roundedValue = Math.ceil(objPriceValue);
+        objPriceValue = roundedValue;
+        var outPutElement = document.getElementById("objPriceValueOne");
+        outPutElement.textContent = (objPriceValue);
+
+        //setbot function
+
+        //setInterval(buyObjAdd, 20) have a path for: function, interval
+
+        return objPriceValue;
+
+    }
+
+}
 
 //obj1
 
 function buyObj() {
-
-    if (finalCount < objPrice) {
-
-        alert("Insuficient Funds")
-
-    }
-
-    if (!(finalCount < objPrice)) {
-
-        finalCount -= objPrice;
-
-        setInterval(buyObjAdd, 20);
-
-        objPrice += 100;
-
-        var outPutElement = document.getElementById("objPrice");
-        outPutElement.textContent = (objPrice)
-
-        botCount += 1;
-
-        var outPutElement = document.getElementById("botCount");
-        outPutElement.textContent = (botCount)
-
-        var outPutElement = document.getElementById("alertBox");
-        outPutElement.textContent = ("Bot 1 Purchased!")
-
-        rateValue += 50;
-
-        var outPutElement = document.getElementById("rate");
-        outPutElement.textContent = (rateValue)
-
-    }
-
-    if (objPrice >= 1500) {
-
-        var outPutElement = document.getElementById("alertBox");
-        outPutElement.textContent = ("Bot 1 Maxed Out!")
-
-        document.getElementById("botBut").style.display = "none";
-
-    }
-
+    objPriceValueOne = buyObjFun(objPriceValueOne);
 }
 
 
@@ -302,46 +281,7 @@ function buyObjAdd() {
 
 function buyObjDos() {
 
-    if (finalCount < objPriceDos) {
-
-        alert("Insuficient Funds")
-
-    }
-
-    if (!(finalCount < objPriceDos)) {
-
-        finalCount -= objPriceDos;
-
-        setInterval(buyObjDosAdd, 1000);
-
-        objPriceDos += 1000;
-
-        var outPutElement = document.getElementById("objPriceDos");
-        outPutElement.textContent = (objPriceDos)
-
-        botCountDos += 1;
-
-        var outPutElement = document.getElementById("botCountDos");
-        outPutElement.textContent = (botCountDos)
-
-        var outPutElement = document.getElementById("alertBox");
-        outPutElement.textContent = ("Bot 2 Purchased!")
-
-        rateValue += 100;
-
-        var outPutElement = document.getElementById("rate");
-        outPutElement.textContent = (rateValue)
-
-    }
-
-    if (objPriceDos >= 10000) {
-
-        var outPutElement = document.getElementById("alertBox");
-        outPutElement.textContent = ("Bot 2 Maxed Out!")
-
-        document.getElementById("botButDos").style.display = "none";
-
-    }
+    objPriceValueDos = buyObjFun(objPriceValueDos);
 
 }
 
@@ -358,49 +298,7 @@ function buyObjDosAdd() {
 //obj3
 
 function buyObjThr() {
-
-    if (finalCount < objPriceThr) {
-
-        alert("Insuficient Funds")
-
-    }
-
-    if (!(finalCount < objPriceThr)) {
-
-        finalCount -= objPriceThr;
-
-        setInterval(buyObjThrAdd, 1000);
-
-        objPriceThr += 5000;
-
-        var outPutElement = document.getElementById("objPriceThr");
-        outPutElement.textContent = (objPriceThr)
-
-        botCountThr += 1;
-
-        var outPutElement = document.getElementById("botCountThr");
-        outPutElement.textContent = (botCountThr)
-
-        var outPutElement = document.getElementById("alertBox");
-        outPutElement.textContent = ("Bot 3 Purchased!")
-
-        rateValue += 1000;
-
-        var outPutElement = document.getElementById("rate");
-        outPutElement.textContent = (rateValue)
-
-    }
-
-    if (objPriceThr >= 60000) {
-
-        var outPutElement = document.getElementById("alertBox");
-        outPutElement.textContent = ("Bot 3 Maxed Out!")
-
-        document.getElementById("botButThr").style.display = "none";
-
-    }
-
-
+    objPriceValueThr = buyObjFun(objPriceValueThr);
 }
 
 
